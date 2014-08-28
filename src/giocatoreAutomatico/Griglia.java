a7c351efd9be24adc856921b66124446bee7677c
Francesca Trudu <francescat-pr2@users.noreply.github.com>
2014-08-28 22:35:25 +0200

    Update Griglia.java

--- src/giocatoreAutomatico/Griglia.java
+++ src/giocatoreAutomatico/Griglia.java
@@ -1,15 +1,19 @@
 package giocatoreAutomatico;
 
 
-/* Griglia.java */
 
-/** le classi che implementano questa interfaccia devono contenere esattamente 16 chiavi.
+
+/* Griglia.java */
+/*le classi che implementano questa interfaccia devono contenere esattamente 16 chiavi.
 Alla Location (0,0), dovrà essere associato il numero (2, oppure 4, oppure 8, ...) associato a quella casella.
 Qualora nella posizione non ci siano numeri, sarà associato il valore -1
-*/
+
+@autor Francesca Trudu
+@autor Nadia Fulgheri*/
 
 
-import game2048.Location;
+
+//import game2048.Location;
 
 public interface Griglia extends java.util.Map<game2048.Location,Integer> { 
 }
