887dae0c716a32dca833891cd7ba02c162360ce0
Francesca Trudu <francescat-pr2@users.noreply.github.com>
2014-08-28 23:15:13 +0200

    Update Griglia.java

--- src/giocatoreAutomatico/player/Griglia.java
+++ src/giocatoreAutomatico/player/Griglia.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package giocatoreAutomatico.player;
 
 import game2048.GameManager;
@@ -16,7 +10,7 @@ import java.util.Map;
 import java.util.Set;
 
 
-/**
+/**Classe 'MyGriglia' che inplementa l'interfaccia 'Griglia'
  *
  * @Francesca Trudu
  * @Nadia Jolanda Fulgheri
