2a99a0ffd0012ce0b9b2e6db49b038e8c93c3104
Francesca Trudu <francescat-pr2@users.noreply.github.com>
2014-08-28 23:17:43 +0200

    Update MyGiocatoreAutomatico.java

--- src/giocatoreAutomatico/player/MyGiocatoreAutomatico.java
+++ src/giocatoreAutomatico/player/MyGiocatoreAutomatico.java
@@ -14,8 +14,25 @@ import java.util.logging.Logger;
 /**
  *
  * @author Francesca Trudu
- * @author Nadia Jolanda Fulgheri
  */
-
 public class MyGiocatoreAutomatico implements GiocatoreAutomatico{
+    //int ALTO=0; int DX=1; int BASSO=2; int SX=3;
+    Griglia gr;
+    
+    
+
+    @Override
+    public int prossimaMossa(Griglia g) {
+        this.gr=g;
+        //viene generato random un int tra 0 e 3
+        int pros = (int) (Math.random() * 4);
+        //System.out.println(pros);
+        
+        
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools Templates.
+        return pros;
+    }
+    
+    
+    
 }
