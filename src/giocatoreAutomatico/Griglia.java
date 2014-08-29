b2afab5b222c8c38cccba4a0bdfc9f673a81fd8e
Francesca Trudu <francescat-pr2@users.noreply.github.com>
2014-08-29 20:33:07 +0200

    Update Griglia.java
    update

--- src/giocatoreAutomatico/Griglia.java
+++ src/giocatoreAutomatico/Griglia.java
@@ -1,15 +1,15 @@
 package giocatoreAutomatico;
 
 
-
-
-/* Griglia.java */
+/* Griglia.java 
+*Interfaccia relativa alla Griglia*/
 /*le classi che implementano questa interfaccia devono contenere esattamente 16 chiavi.
 Alla Location (0,0), dovrà essere associato il numero (2, oppure 4, oppure 8, ...) associato a quella casella.
 Qualora nella posizione non ci siano numeri, sarà associato il valore -1
 
 @autor Francesca Trudu
-@autor Nadia Fulgheri*/
+@autor Nadia Fulgheri
+*/
 
 
 
